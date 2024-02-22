bbb32bbc7ccf17d3fe0bafff375e5a69d39271b8
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2024-02-22 21:16:23 +0300

    Nuget packages versions were updated

---
+++ CulturalShare.Common.DB/Entites/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace CulturalShare.Common.DB.Entites;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+}
