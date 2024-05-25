90faf1fac67752002f5f2bf38784ef6e82ef39fc
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2024-05-25 15:18:16 +0300

    CUL-13 GraylogConfiguration was added

---
+++ CulturalShare.Common.Helper/Extensions/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace CulturalShare.Common.Helper.Extensions;
+
+public static class EnumExtensions
+{
+    public static T ToEnum<T>(this string value) where T : struct, Enum
+    {
+        if (Enum.TryParse(value, true, out T result))
+        {
+            return result;
+        }
+        throw new ArgumentException($"Unable to convert '{value}' to enum {typeof(T).Name}");
+    }
+}
